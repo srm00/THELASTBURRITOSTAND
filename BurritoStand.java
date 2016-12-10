7147029ae5ce2ebba16250aea1bf0a5eb9362385
srm00 <seanrm007@gmail.com>
2016-12-10 18:32:01 -0500

    Create BurritoStand.java

---
+++ BurritoStand.java
@@ -0,0 +1,20 @@
+public class BurritoStand {
+	
+	public static void main(String[] args){
+		//Keeps track of all orders
+	    //Takes Orders
+	    //Modifies/manipulates all other classes
+	}
+	
+	public static void takeOrder(){
+		
+	}
+	
+	public static void makeOrder(){
+		
+	}
+	
+	public static void checkout(){
+		
+	}
+}
