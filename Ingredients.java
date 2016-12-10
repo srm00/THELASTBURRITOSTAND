1755126b3212d0ea38b3f27c9f0f6bdc503477a9
srm00 <seanrm007@gmail.com>
2016-12-10 18:58:50 -0500

    Create Ingredients.java

---
+++ Ingredients.java
@@ -0,0 +1,26 @@
+public class Ingredients {
+	private String name;
+	private double price;
+	
+	//constructor, takes in a name and a price
+	public Ingredients(String str, double x){
+		this.setName(str);
+		this.setPrice(x);
+	}
+
+	public void setName(String str) {
+		name = str;
+	}
+	
+	public String getName(){
+		return name; 
+	}
+	
+	public void setPrice(double x){
+		price = x;
+	}
+	
+	public double getPrice(){
+		return price;
+	}
+}
