08ca2274030119091d5ed9d6f1f9126cd68a291d
srm00 <seanrm007@gmail.com>
2016-12-10 19:59:34 -0500

    Update BurritoStand.java

--- BurritoStand.java
+++ BurritoStand.java
@@ -6,31 +6,31 @@ public class BurritoStand {
 	    //Modifies/manipulates all other classes
 		
 		
-		//Ingredients List
- 	Ingredients wheat = new Ingredients("Wheat", 1.0);
-	Ingredients white = new Ingredietns("White", 0.5);
-	Ingredients bowl = new Ingredients("Bowl", 0.3); 
-	Ingredients chicken = new Ingredients("Chicken", 4.0); 
-	Ingredients pork = new Ingredients("Pork", 5.0); 
-	Ingredients chorizo = new Ingredients("Chorizo", 5.0); 
-	Ingredients groundBeef = new Ingredients("Ground Beef", 4.5);
-	Ingredients carneAsada = new Ingredients("Carne Asada", 5.0);
-	Ingredients vegetables = new Ingredients("Vegetables", 3.5); 
-	Ingredients whiteRice = new Ingredients("Whte Rice", 1.10);
-	Ingredietns brownRice = new Ingredients("Brown Rice", 1.5); 
-	Ingredients blackBeans = new Ingredients("Black Beans", 1.0);
-	Ingredients pintoBeans = new Ingredients("Pinto Beans", 1.0); 
-	Ingredietns refriedBeans = new Ingredients("Refried Beans", 1.0); 
-	Ingredients picoDeGallo = new Ingredients("Pico de gallo", 0.4); 
-	Ingredients mild = new Ingredients("Mild", 0.4); 
-	Ingredients medium = new Ingredients("Medium", 0.4); 
-	Ingredients hot = new Ingredients("Hot", 0.4); 
-	Ingredients lettuce = new Ingredients("Lettuce", 0.75); 
-	Ingredients tomato = new Ingredients("Tomato", 0.75); 
-	Ingredients pepersAndOnion = new Ingredients("Peppers & Onion",  0.75); 
-	Ingredients guacamole = new Ingredients("Guacamole", 1.90); 
-	Ingredients sourCream = new Ingredients("Sour Cream", 0.90); 
-	Ingredients cheese = new Ingredients("Cheese" , 0.90); 
+		//Ingredient List
+ 	Ingredient wheat = new Ingredient("Wheat", 1.0);
+	Ingredient white = new Ingredietns("White", 0.5);
+	Ingredient bowl = new Ingredient("Bowl", 0.3); 
+	Ingredient chicken = new Ingredient("Chicken", 4.0); 
+	Ingredient pork = new Ingredient("Pork", 5.0); 
+	Ingredient chorizo = new Ingredient("Chorizo", 5.0); 
+	Ingredient groundBeef = new Ingredient("Ground Beef", 4.5);
+	Ingredient carneAsada = new Ingredient("Carne Asada", 5.0);
+	Ingredient vegetables = new Ingredient("Vegetables", 3.5); 
+	Ingredient whiteRice = new Ingredient("Whte Rice", 1.10);
+	Ingredietns brownRice = new Ingredient("Brown Rice", 1.5); 
+	Ingredient blackBeans = new Ingredient("Black Beans", 1.0);
+	Ingredient pintoBeans = new Ingredient("Pinto Beans", 1.0); 
+	Ingredietns refriedBeans = new Ingredient("Refried Beans", 1.0); 
+	Ingredient picoDeGallo = new Ingredient("Pico de gallo", 0.4); 
+	Ingredient mild = new Ingredient("Mild", 0.4); 
+	Ingredient medium = new Ingredient("Medium", 0.4); 
+	Ingredient hot = new Ingredient("Hot", 0.4); 
+	Ingredient lettuce = new Ingredient("Lettuce", 0.75); 
+	Ingredient tomato = new Ingredient("Tomato", 0.75); 
+	Ingredient pepersAndOnion = new Ingredient("Peppers & Onion",  0.75); 
+	Ingredient guacamole = new Ingredient("Guacamole", 1.90); 
+	Ingredient sourCream = new Ingredient("Sour Cream", 0.90); 
+	Ingredient cheese = new Ingredient("Cheese" , 0.90); 
 	}
 	
 	public static void takeOrder(){
