3e885efc62a338670b943ccb2ebcb703f24d1b70
srm00 <seanrm007@gmail.com>
2016-12-12 13:08:28 -0500

    Update BurritoStand.java

--- BurritoStand.java
+++ BurritoStand.java
@@ -1,48 +1,67 @@
-
+import java.util.Scanner;
 public class BurritoStand {
 	
-	   //Keeps track of all orders, Takes Orders, Modifies and manipulates all other classes
 	public static void main(String[] args){
-		Order o1 = new Order(); 
-		o1.getOrder(); 
-		//o1.checkout(); 
-	
+		//Keeps track of all orders
+	    //Takes Orders
+	    //Modifies/manipulates all other classes
 		
 		
 		//Ingredient List
-		Ingredient wheat = new Ingredient("Wheat", 1.0);
-		Ingredient white = new Ingredient("White", 0.5);
-		Ingredient bowl = new Ingredient("Bowl", 0.3); 
-		Ingredient chicken = new Ingredient("Chicken", 4.0); 
-		Ingredient pork = new Ingredient("Pork", 5.0); 
-		Ingredient chorizo = new Ingredient("Chorizo", 5.0); 
-		Ingredient groundBeef = new Ingredient("Ground Beef", 4.5);
-		Ingredient carneAsada = new Ingredient("Carne Asada", 5.0);
-		Ingredient vegetables = new Ingredient("Vegetables", 3.5); 
-		Ingredient whiteRice = new Ingredient("Whte Rice", 1.10);
-		Ingredient brownRice = new Ingredient("Brown Rice", 1.5); 
-		Ingredient blackBeans = new Ingredient("Black Beans", 1.0);
-		Ingredient pintoBeans = new Ingredient("Pinto Beans", 1.0); 
-		Ingredient refriedBeans = new Ingredient("Refried Beans", 1.0); 
-		Ingredient picoDeGallo = new Ingredient("Pico de gallo", 0.4); 
-		Ingredient mild = new Ingredient("Mild", 0.4); 
-		Ingredient medium = new Ingredient("Medium", 0.4); 
-		Ingredient hot = new Ingredient("Hot", 0.4); 
-		Ingredient lettuce = new Ingredient("Lettuce", 0.75); 
-		Ingredient tomato = new Ingredient("Tomato", 0.75); 
-		Ingredient pepersAndOnion = new Ingredient("Peppers & Onion",  0.75); 
-		Ingredient guacamole = new Ingredient("Guacamole", 1.90); 
-		Ingredient sourCream = new Ingredient("Sour Cream", 0.90); 
-		Ingredient cheese = new Ingredient("Cheese" , 0.90); 
+ 	Ingredient wheat = new Ingredient("Wheat", 1.0);
+	Ingredient white = new Ingredient("White", 0.5);
+	Ingredient bowl = new Ingredient("Bowl", 0.3); 
+	Ingredient chicken = new Ingredient("Chicken", 4.0); 
+	Ingredient pork = new Ingredient("Pork", 5.0); 
+	Ingredient chorizo = new Ingredient("Chorizo", 5.0); 
+	Ingredient groundBeef = new Ingredient("Ground Beef", 4.5);
+	Ingredient carneAsada = new Ingredient("Carne Asada", 5.0);
+	Ingredient vegetables = new Ingredient("Vegetables", 3.5); 
+	Ingredient whiteRice = new Ingredient("Whte Rice", 1.10);
+	Ingredient brownRice = new Ingredient("Brown Rice", 1.5); 
+	Ingredient blackBeans = new Ingredient("Black Beans", 1.0);
+	Ingredient pintoBeans = new Ingredient("Pinto Beans", 1.0); 
+	Ingredient refriedBeans = new Ingredient("Refried Beans", 1.0); 
+	Ingredient picoDeGallo = new Ingredient("Pico de gallo", 0.4); 
+	Ingredient mild = new Ingredient("Mild", 0.4); 
+	Ingredient medium = new Ingredient("Medium", 0.4); 
+	Ingredient hot = new Ingredient("Hot", 0.4); 
+	Ingredient lettuce = new Ingredient("Lettuce", 0.75); 
+	Ingredient tomato = new Ingredient("Tomato", 0.75); 
+	Ingredient pepersAndOnion = new Ingredient("Peppers & Onion",  0.75); 
+	Ingredient guacamole = new Ingredient("Guacamole", 1.90); 
+	Ingredient sourCream = new Ingredient("Sour Cream", 0.90); 
+	Ingredient cheese = new Ingredient("Cheese" , 0.90); 
 	}
 	
-
+	public static void takeOrder(){
+		//Asks number of Burritos and the initializes the BurritoList[] as an array of Burritos of that size.
+	}
 	
+	public static void makeOrder(){
+		
+	}
 	
+	 
 	
-	
-	
-	
-	
-
+	public static int takeUserInput(int maxChoices){
+		Scanner keyboard = new Scanner(System.in);
+		boolean validInput = false;
+		int x=0;
+		while(!validInput){
+			if(keyboard.hasNextInt()){
+				x=keyboard.nextInt();
+				if(x <= maxChoices){
+					validInput = true;
+				}else{
+					System.out.println("Please input a valid choice from 0 to " + maxChoices);
+				}
+			}else{
+				System.out.println("Please input an integer");
+			}
+			keyboard.nextLine();
+		}
+		keyboard.close();
+		return x;
+	}
 }
