b66165c7f2ba3d428e1ee3d27ffd62748c70e223
emmeline2 <elp65@case.edu>
2016-12-11 21:45:05 -0500

    Update BurritoStand.java

--- BurritoStand.java
+++ BurritoStand.java
@@ -1,36 +1,36 @@
 public class BurritoStand {
 	
 	public static void main(String[] args){
-		//Keeps track of all orders
+	    //Keeps track of all orders
 	    //Takes Orders
 	    //Modifies/manipulates all other classes
 		
 		
 		//Ingredient List
- 	Ingredient wheat = new Ingredient("Wheat", 1.0);
-	Ingredient white = new Ingredietns("White", 0.5);
-	Ingredient bowl = new Ingredient("Bowl", 0.3); 
-	Ingredient chicken = new Ingredient("Chicken", 4.0); 
-	Ingredient pork = new Ingredient("Pork", 5.0); 
-	Ingredient chorizo = new Ingredient("Chorizo", 5.0); 
-	Ingredient groundBeef = new Ingredient("Ground Beef", 4.5);
-	Ingredient carneAsada = new Ingredient("Carne Asada", 5.0);
-	Ingredient vegetables = new Ingredient("Vegetables", 3.5); 
-	Ingredient whiteRice = new Ingredient("Whte Rice", 1.10);
-	Ingredietns brownRice = new Ingredient("Brown Rice", 1.5); 
-	Ingredient blackBeans = new Ingredient("Black Beans", 1.0);
-	Ingredient pintoBeans = new Ingredient("Pinto Beans", 1.0); 
-	Ingredietns refriedBeans = new Ingredient("Refried Beans", 1.0); 
-	Ingredient picoDeGallo = new Ingredient("Pico de gallo", 0.4); 
-	Ingredient mild = new Ingredient("Mild", 0.4); 
-	Ingredient medium = new Ingredient("Medium", 0.4); 
-	Ingredient hot = new Ingredient("Hot", 0.4); 
-	Ingredient lettuce = new Ingredient("Lettuce", 0.75); 
-	Ingredient tomato = new Ingredient("Tomato", 0.75); 
-	Ingredient pepersAndOnion = new Ingredient("Peppers & Onion",  0.75); 
-	Ingredient guacamole = new Ingredient("Guacamole", 1.90); 
-	Ingredient sourCream = new Ingredient("Sour Cream", 0.90); 
-	Ingredient cheese = new Ingredient("Cheese" , 0.90); 
+		Ingredient wheat = new Ingredient("Wheat", 1.0);
+		Ingredient white = new Ingredietns("White", 0.5);
+		Ingredient bowl = new Ingredient("Bowl", 0.3); 
+		Ingredient chicken = new Ingredient("Chicken", 4.0); 
+		Ingredient pork = new Ingredient("Pork", 5.0); 
+		Ingredient chorizo = new Ingredient("Chorizo", 5.0); 
+		Ingredient groundBeef = new Ingredient("Ground Beef", 4.5);
+		Ingredient carneAsada = new Ingredient("Carne Asada", 5.0);
+		Ingredient vegetables = new Ingredient("Vegetables", 3.5); 
+		Ingredient whiteRice = new Ingredient("Whte Rice", 1.10);
+		Ingredietns brownRice = new Ingredient("Brown Rice", 1.5); 
+		Ingredient blackBeans = new Ingredient("Black Beans", 1.0);
+		Ingredient pintoBeans = new Ingredient("Pinto Beans", 1.0); 
+		Ingredietns refriedBeans = new Ingredient("Refried Beans", 1.0); 
+		Ingredient picoDeGallo = new Ingredient("Pico de gallo", 0.4); 
+		Ingredient mild = new Ingredient("Mild", 0.4); 
+		Ingredient medium = new Ingredient("Medium", 0.4); 
+		Ingredient hot = new Ingredient("Hot", 0.4); 
+		Ingredient lettuce = new Ingredient("Lettuce", 0.75); 
+		Ingredient tomato = new Ingredient("Tomato", 0.75); 
+		Ingredient pepersAndOnion = new Ingredient("Peppers & Onion",  0.75); 
+		Ingredient guacamole = new Ingredient("Guacamole", 1.90); 
+		Ingredient sourCream = new Ingredient("Sour Cream", 0.90); 
+		Ingredient cheese = new Ingredient("Cheese" , 0.90); 
 	}
 	
 	public static void getOrder(){
@@ -50,7 +50,6 @@ public class BurritoStand {
 			System.out.println("Here is this completed burrito. It contains the following ingredients: "); 
 			printBurrito(x); 
 		}
-	
 	}
 	
 	
@@ -65,6 +64,8 @@ public class BurritoStand {
 		}
 		System.out.println("Your total is " + totalPrice + "$. Thank you for coming!"); 
 	}
+	
+	
 	
 	public static void main (String [] args) 
 	{
